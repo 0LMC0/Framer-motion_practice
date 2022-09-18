a153a2cf3ae2b0932b662a434aa48cf8c6df1aa0
unknown <lautycaceresss871@gmail.com>
2022-09-17 21:17:03 -0300

    note err

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Box1 from './components/box1/box1';
+import Box1 from './components/box1/Box1';
 import Box2 from './components/box2/Box2';
 import Box3 from './components/box3/Box3';
 import Box4 from './components/box4/Box4';
